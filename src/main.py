b127e3a4c15d84d7cb56836b9ede15f9d8824850
tubu-Git <shouki_0118@au.com>
2025-11-20 15:45:13 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,21 +3,7 @@ from pathlib import Path
 
 from PySide6.QtWidgets import QApplication, QVBoxLayout, QWidget
 
-current_dir = Path(__file__).resolve().parent
-
-project_root = current_dir.parent
-
-sys.path.append(str(current_dir))
-sys.path.append(str(project_root))
-
-try:
-    from TranscribeWidget import TranscribeWidget
-except ImportError:
-    try:
-        from widgets.TranscribeWidget import TranscribeWidget
-    except ImportError:
-        from src.widgets.TranscribeWidget import TranscribeWidget
-
+from TranscribeWidget import TranscribeWidget
 from mod.paths import DIR_OUT
 
 
@@ -27,15 +13,14 @@ class MainWindow(QWidget):
         self.__define_layout()
 
     def __define_layout(self) -> None:
+        self.setWindowTitle("title")
         self.resize(400, 300)
 
         layout = QVBoxLayout()
         
-        # ウィジェットの配置
         self.transcribe_widget = TranscribeWidget()
         layout.addWidget(self.transcribe_widget)
 
-        # 動作確認用: 録音ファイルがあればセットする
         test_audio_path = DIR_OUT
         
         if test_audio_path.exists():
