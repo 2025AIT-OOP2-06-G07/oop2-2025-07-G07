60ca3d0b8850378a4114adcdfdc596db42cfd12f
織田 <ryuchan.0413to@gmail.com>
2025-11-20 15:17:18 +0900

    作業終了

--- src/widgets/save.py
+++ src/widgets/save.py
@@ -12,22 +12,13 @@ class SaveWidget(QWidget):
     def __define_layout(self) -> None:
         layout = QVBoxLayout()
 
-        button1 = QPushButton("ボタン 1")
-        button2 = QPushButton("ボタン 2")
         button3 = QPushButton("保存")
 
-        button1.clicked.connect(self.__on_click_button)
-        button2.clicked.connect(self.__on_click_button)
         button3.clicked.connect(self.__on_click_button3)
 
-        layout.addWidget(button1)
-        layout.addWidget(button2)
         layout.addWidget(button3)
 
         self.setLayout(layout)
-
-    def __on_click_button(self) -> None:
-        print("ボタンがクリックされました")
 
     def __on_click_button3(self) -> None:
         save_with_timestamp('transcribed_text', DIR_OUT)
